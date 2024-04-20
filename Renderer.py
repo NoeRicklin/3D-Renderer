a9e110898ec2e1d41031a816559823532f340089
Glueckskeks4 <noe.ricklin@outlook.de>
2024-04-20 19:48:19 +0200

    Created basic functions to simplify often used features

--- Renderer.py
+++ Renderer.py
@@ -9,7 +9,25 @@ WIDTH, HEIGHT = 1024, 720
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 
 
-while True:
+def va(vector1, vector2, sign=1):   # vector-addition
+    summed_vector = [vector1[i] + vector2[i] for i in range(len(vector1))]
+    return summed_vector
+
+
+def sm(scalar, vector):     # scalar-multiplication of a vector
+    scaled_vector = [scalar * element for element in vector]
+    return scaled_vector
+
+
+def drawline(start, end, color="White", width=2):
+    pg.draw.line(screen, color, start, end, width)
+
+
+def drawpoint(position, color="White", size=4):
+    pg.draw.circle(screen, color, position, size)
+
+
+while True:     # main loop in which everything happens
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
