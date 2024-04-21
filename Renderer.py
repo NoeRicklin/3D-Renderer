f5138a1362ec83664901d170c627ce46443a68ac
Glueckskeks4 <noe.ricklin@outlook.de>
2024-04-21 21:53:43 +0200

    Changed approach for the projection

--- Renderer.py
+++ Renderer.py
@@ -6,25 +6,26 @@ import numpy as np
 pg.init()
 clock = pg.time.Clock()
 
-WIDTH, HEIGHT = 1024, 720
-screen = pg.display.set_mode((WIDTH, HEIGHT))
+dims = 1024, 720
+screen = pg.display.set_mode(dims)
 
 
 # camera settings
 cam_pos = (512, 500)
 cam_dir = (0, -1)
+cam_norm = (0, 0)
 cam_fov = 80    # horizontal field-of-view angle in degrees
-viewplain_dis = 50
+viewplane_dis = 50
 
 speed = 5
-rot_speed = .1
+rot_speed = .05
 
 vertices = [(300, 400), (500, 120), (650, 100)]
 vert_colors = ["Blue", "Red", "Green"]
 edges = [(0, 1), (1, 2)]    # contains the indexes of the vertices in the vertices-array that share an edge
 
 
-def move_cam(pos, dir):     # camera controller to move the camera around with the keyboard
+def move_cam(current_pos, current_dir):     # camera controller to move the camera around with the keyboard
     velocity = 0
     rotation = 0
 
@@ -38,9 +39,10 @@ def move_cam(pos, dir):     # camera controller to move the camera around with t
     elif key_list[pg.K_d]:
         rotation = rot_speed
 
-    cam_dir_new = rot_vec(dir, rotation)
-    cam_pos_new = va(pos, sm(velocity, dir))
-    return cam_pos_new, cam_dir_new
+    cam_dir_new = rot_vec(current_dir, rotation)
+    cam_norm_new = rot_vec(cam_dir_new, np.pi/2)
+    cam_pos_new = va(current_pos, sm(velocity, cam_dir_new))
+    return cam_pos_new, cam_dir_new, cam_norm_new
 
 
 def draw_cam():     # displays the camera and its FOV legs to show its direction
@@ -55,28 +57,32 @@ def draw_cam():     # displays the camera and its FOV legs to show its direction
     drawline(cam_pos, fov_leg2_end, "Yellow")
 
 
-def project_to_screen(point):   # returns the coordinates of the point after being projected to the viewplain
-    cam_space_point = va(point, cam_pos, -1)
-    point_angle = np.arccos(np.dot(cam_space_point, cam_dir) / (magn(cam_space_point)))
+def project_to_screen(point):
+    # the location of the point, if you imagine the camera to be at the center of a coordinate system, always pointing
+    # in the y direction (it calculates the linear-combination of the point to the cam-dir and its normalvector)
+    cam_space_point = tuple(np.linalg.solve([cam_norm, cam_dir], va(point, cam_pos, -1)))
 
-    # checks if the point would be visable and ignores it if not
-    if abs(point_angle) < np.deg2rad(cam_fov/2):
-        if magn(cam_space_point) >= viewplain_dis:
-            stretch_factor = viewplain_dis / (np.dot(cam_space_point, cam_dir))
-            scaled_point = va(cam_pos, sm(stretch_factor, cam_space_point))
-            return scaled_point
+    cam_and_point_dot = np.dot(cam_space_point, (0, 1))
+    point_angle = np.arccos(cam_and_point_dot / (magn(cam_space_point)))
+
+    if point_angle < np.deg2rad(cam_fov/2) and magn(cam_space_point) > viewplane_dis:   # checks if the point is visable
+        stretch_factor = viewplane_dis / cam_and_point_dot
+        cam_space_proj_point = sm(stretch_factor, cam_space_point)
+        return va(cam_space_proj_point, sm(.5, dims))
 
 
 def display_verts(show_global_vert=True):
     for vertice in range(len(vertices)):
-        if show_global_vert: drawpoint(vertices[vertice], vert_colors[vertice])
+        if show_global_vert:
+            drawpoint(vertices[vertice], vert_colors[vertice])
         drawpoint(project_to_screen(vertices[vertice]), vert_colors[vertice])
 
 
 def display_edges(show_global_edge=True):
     for edge in edges:
         vert1, vert2 = vertices[edge[0]], vertices[edge[1]]
-        if show_global_edge: drawline(vert1, vert2)
+        if show_global_edge:
+            drawline(vert1, vert2)
         drawline(project_to_screen(vert1), project_to_screen(vert2))
 
 
@@ -119,7 +125,16 @@ while True:     # main loop in which everything happens
             exit()
     screen.fill("Black")
 
-    cam_pos, cam_dir = move_cam(cam_pos, cam_dir)
+    # temporary visual of central camera to test if camera-space points are working properly
+    drawpoint(sm(.5, dims), "Yellow", 7)
+    fov_leg1_vec = rot_vec((0, 1), np.deg2rad(cam_fov / 2))
+    fov_leg1_end = va(sm(.5, dims), sm(2000, fov_leg1_vec))
+    drawline(sm(.5, dims), fov_leg1_end, "Yellow")
+    fov_leg2_vec = rot_vec((0, 1), -np.deg2rad(cam_fov / 2))
+    fov_leg2_end = va(sm(.5, dims), sm(2000, fov_leg2_vec))
+    drawline(sm(.5, dims), fov_leg2_end, "Yellow")
+
+    cam_pos, cam_dir, cam_norm = move_cam(cam_pos, cam_dir)
     draw_cam()
 
     display_verts()
