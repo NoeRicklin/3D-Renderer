b62d561ac752c5f1051c1ef4625c0581a40a059b
noe.ricklin@outlook.de <W*w4rK7#bjN8pfU>
2024-04-25 21:39:29 +0200

    Added Camera- and Object-Class and converted the current instances to them

--- Logbuch.md
+++ Logbuch.md
@@ -3,4 +3,7 @@
 * 2024-04-19: Erstellen des Repositories und grundlegendes Aufsetzen der Python-Datei.
 * 2024-04-21: 2D Projektion von Punkten auf eine Kamera erstellt, um die grundsätziche Funktionsweise in 2D zu testen.
 * 2024-04-22: 3D Projektion eines Würfelsgitters und mit tastatur beweg- und drehbare Kamera
-* 2024-04-24: Möglichkeit die Kamera rauf und runter zu schwenken und mit der Maus zu bedienen eingeführt
+* 2024-04-24: Möglichkeit die Kamera rauf und runter zu schwenken und mit der Maus zu bedienen eingeführt.
+* 2024-04-25: Objekt- und Kameraklassen eingeführt um mehrere Instanzen davon nutzen zu können wenn benötigt. 
+Da jetzt auch viele Objekt- oder Kamera bezogene Funktionen in die Klassen gesteckt
+werden konnten ist der Code jetzt etwas übersichtlicher.
--- Renderer.py
+++ Renderer.py
@@ -7,10 +7,17 @@ import pygame as pg
 import win32api
 import win32con
 
+"""
+CONTROLS:
+Camera Movement: WASD
+Camera Rotation: mouse
+"""
+
 # pygame settings
 dims = (1080, 720)
 window_pos = ((1920-dims[0])/2, (1080-dims[1])/2)
 os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % window_pos
+mouse_pos = (0, 0)
 
 pg.init()
 stime = time.time()
@@ -18,28 +25,128 @@ clock = pg.time.Clock()
 
 screen = pg.display.set_mode(dims)
 
-# camera settings
-cam_pos = (0, 0, 100)
-cam_right = (1, 0, 0)
-cam_up = (0, 1, 0)
-cam_dir = (0, 0, 1)
-cam_fov = 80    # horizontal field-of-view angle in degrees
-viewplane_dis = 50  # distance of the viewplane relative to the camera
+models = []
 
-mouse_pos = (0, 0)
 
-speed = 5
-rot_speed = .05
+class Model:
+    def __init__(self, verts,  lines, vcols=None, ecols=None):
+        if vcols is None:
+            vcols = {}
+        if ecols is None:
+            ecols = {}
+        self.vertices = verts
+        self.edges = lines  # contains the indexes of the vertices in the that share an edge
 
-# vertices settings
-vertices = [(-50, -50, 300), (50, -50, 300), (50, 50, 300), (-50, 50, 300),
-            (-50, -50, 400), (50, -50, 400), (50, 50, 400), (-50, 50, 400)]
-vert_colors = {1: "Blue"}
+        self.vert_colors = vcols    # color of the points
+        self.edge_colors = ecols    # color of the edges
 
-edges = [(0, 1), (1, 2), (2, 3), (3, 0),
-         (0, 4), (1, 5), (2, 6), (3, 7),
-         (4, 5), (5, 6), (6, 7), (7, 4)]  # contains the indexes of the vertices in the that share an edge
-edge_colors = {2: "Blue"}
+        models.append(self)
+
+
+class Camera:
+    def __init__(self, pos, mouse_control=True):
+        self.pos = pos
+        self.right = (1, 0, 0)
+        self.up = (0, 1, 0)
+        self.dir = (0, 0, 1)
+        self.fov = 80  # horizontal field-of-view angle in degrees
+        self.viewplane_dis = 50  # distance of the viewplane relative to the camera
+        self.speed = 5
+        self.rot_speed = .05
+        self.mouse_control = mouse_control
+
+    def display_verts(self, obj, show_global_vert=False):
+        for vertex_index in range(len(obj.vertices)):
+            try:
+                color = obj.vert_colors[vertex_index]
+            except KeyError:
+                color = "White"
+            if show_global_vert:
+                drawpoint(obj.vertices[vertex_index], color)
+            drawpoint(self.project_to_screen(obj.vertices[vertex_index]), color)
+
+    def display_edges(self, obj, show_global_edge=False):
+        for edge_index in range(len(obj.edges)):
+            try:
+                color = obj.edge_colors[edge_index]
+            except KeyError:
+                color = "White"
+            vert1, vert2 = obj.vertices[obj.edges[edge_index][0]], obj.vertices[obj.edges[edge_index][1]]
+            drawline(self.project_to_screen(vert1), self.project_to_screen(vert2), color)
+            if show_global_edge:
+                drawline(vert1, vert2, color)
+
+    def project_to_screen(self, point):
+        # the location of the point, if you imagine the camera to be at the center of a coordinate system, always 
+        # pointing in the y direction (it calculates the l-combination of the point to the cam-dir and its normalvector)
+        # converts the cameravectors into a list so that they can be used in the linear-combination algorhythm
+        cam_vecs = np.array([[self.right[i], self.up[i], self.dir[i]] for i in range(3)])
+        cam_space_point = np.linalg.solve(cam_vecs, va(point, self.pos, sign=-1))
+
+        cam_and_point_dot = np.dot(cam_space_point, (0, 0, 1))
+        point_angle = np.arccos(cam_and_point_dot / (magn(cam_space_point)))
+
+        if point_angle < np.pi / 2:  # checks if the point is visable
+            stretch_factor = self.viewplane_dis / cam_and_point_dot
+            cam_space_proj_point = sm(stretch_factor, cam_space_point)[:2]  # reduces the dimension of the points to 2D
+            cam_space_proj_point = (cam_space_proj_point[0], -1 * cam_space_proj_point[1])  # flips image (pygame-BS)
+            cam_space_proj_point = va(sm(7, cam_space_proj_point), sm(.5, dims))  # centers points on screen
+            return cam_space_proj_point
+
+    def move_cam(self):  # camera controller to move the camera around with the keyboard
+        velocity = [0, 0, 0]
+        rotation = [0, 0]  # only 2 values because the camera doesn't need to turn around the z axis
+        key_list = pg.key.get_pressed()
+        # movement forward/backward
+        if key_list[pg.K_w]:
+            velocity[2] = self.speed
+        elif key_list[pg.K_s]:
+            velocity[2] = -self.speed
+        # movement right/left
+        if key_list[pg.K_d]:
+            velocity[0] = self.speed
+        elif key_list[pg.K_a]:
+            velocity[0] = -self.speed
+        # movement up/down
+        if key_list[pg.K_SPACE]:
+            velocity[1] = self.speed
+        elif key_list[pg.K_LSHIFT]:
+            velocity[1] = -self.speed
+
+        if self.mouse_control:
+            rotation = sm(0.005, move_mouse())
+        else:
+            # rotation left/right
+            if key_list[pg.K_LEFT]:
+                rotation[1] = -self.rot_speed
+            elif key_list[pg.K_RIGHT]:
+                rotation[1] = self.rot_speed
+            # rotation up/downright
+            if key_list[pg.K_DOWN]:
+                rotation[0] = -self.rot_speed
+            elif key_list[pg.K_UP]:
+                rotation[0] = self.rot_speed
+
+        # apply x-axis rotation
+        self.up, self.dir = rot_vec(self.up, rotation[0], self.right), rot_vec(self.dir, rotation[0], self.right)
+        # apply y-axis rotation (rotation around the global y-axis so that it doesn't rotate around the z-axis
+        self.right = rot_vec(self.right, rotation[1], (0, 1, 0))
+        self.up = rot_vec(self.up, rotation[1], (0, 1, 0))
+        self.dir = rot_vec(self.dir, rotation[1], (0, 1, 0))
+
+        move_vec = va(sm(velocity[0], self.right), sm(velocity[1], self.up), sm(velocity[2], self.dir))
+        self.pos = va(self.pos, move_vec)
+
+    def draw_cam(self):  # displays the camera and its FOV legs to show its direction (only used in 2D)
+        drawpoint(self.pos, "Yellow", 7)
+
+        fov_leg1_vec = rot_vec(self.dir, np.deg2rad(self.fov / 2))
+        fov_leg1_end = va(self.pos, sm(2000, fov_leg1_vec))
+        drawline(self.pos, fov_leg1_end, "Yellow")
+
+        fov_leg2_vec = rot_vec(self.dir, -np.deg2rad(self.fov / 2))
+        fov_leg2_end = va(self.pos, sm(2000, fov_leg2_vec))
+        drawline(self.pos, fov_leg2_end, "Yellow")
 
 
 def move_mouse():
@@ -50,105 +157,6 @@ def move_mouse():
                          int(center[1]/1080*65535.0+1))
     mouse_pos = current_mouse_pos
     return delta_mouse
-
-
-def move_cam(mouse_control=True):     # camera controller to move the camera around with the keyboard
-    global cam_pos, cam_right, cam_up, cam_dir
-    velocity = [0, 0, 0]
-    rotation = [0, 0]   # only 2 values because the camera doesn't need to turn around the z axis
-    key_list = pg.key.get_pressed()
-    # movement forward/backward
-    if key_list[pg.K_w]:
-        velocity[2] = speed
-    elif key_list[pg.K_s]:
-        velocity[2] = -speed
-    # movement right/left
-    if key_list[pg.K_d]:
-        velocity[0] = speed
-    elif key_list[pg.K_a]:
-        velocity[0] = -speed
-    # movement up/down
-    if key_list[pg.K_SPACE]:
-        velocity[1] = speed
-    elif key_list[pg.K_LSHIFT]:
-        velocity[1] = -speed
-
-    if mouse_control:
-        rotation = sm(0.005, move_mouse())
-    else:
-        # rotation left/right
-        if key_list[pg.K_LEFT]:
-            rotation[1] = -rot_speed
-        elif key_list[pg.K_RIGHT]:
-            rotation[1] = rot_speed
-        # rotation up/downright
-        if key_list[pg.K_DOWN]:
-            rotation[0] = -rot_speed
-        elif key_list[pg.K_UP]:
-            rotation[0] = rot_speed
-
-    # apply x-axis rotation
-    cam_up, cam_dir = rot_vec(cam_up, rotation[0], cam_right), rot_vec(cam_dir, rotation[0], cam_right)
-    # apply y-axis rotation (rotation around the global y-axis so that it doesn't rotate around the z-axis
-    cam_right = rot_vec(cam_right, rotation[1], (0, 1, 0))
-    cam_up = rot_vec(cam_up, rotation[1], (0, 1, 0))
-    cam_dir = rot_vec(cam_dir, rotation[1], (0, 1, 0))
-
-    move_vec = va(sm(velocity[0], cam_right), sm(velocity[1], cam_up), sm(velocity[2], cam_dir))
-    cam_pos = va(cam_pos, move_vec)
-
-
-def draw_cam():     # displays the camera and its FOV legs to show its direction (only used in 2D)
-    drawpoint(cam_pos, "Yellow", 7)
-
-    fov_leg1_vec = rot_vec(cam_dir, np.deg2rad(cam_fov/2))
-    fov_leg1_end = va(cam_pos, sm(2000, fov_leg1_vec))
-    drawline(cam_pos, fov_leg1_end, "Yellow")
-
-    fov_leg2_vec = rot_vec(cam_dir, -np.deg2rad(cam_fov / 2))
-    fov_leg2_end = va(cam_pos, sm(2000, fov_leg2_vec))
-    drawline(cam_pos, fov_leg2_end, "Yellow")
-
-
-def project_to_screen(point):
-    # the location of the point, if you imagine the camera to be at the center of a coordinate system, always pointing
-    # in the y direction (it calculates the linear-combination of the point to the cam-dir and its normalvector)
-    # converts the cameravectors into a list so that they can be used in the linear-combination algorhythm
-    cam_vecs = np.array([[cam_right[i], cam_up[i], cam_dir[i]] for i in range(3)])
-    cam_space_point = np.linalg.solve(cam_vecs, va(point, cam_pos, sign=-1))
-
-    cam_and_point_dot = np.dot(cam_space_point, (0, 0, 1))
-    point_angle = np.arccos(cam_and_point_dot / (magn(cam_space_point)))
-
-    if point_angle < np.pi/2:   # checks if the point is visable
-        stretch_factor = viewplane_dis / cam_and_point_dot
-        cam_space_proj_point = sm(stretch_factor, cam_space_point)[:2]  # reduces the dimension of the points to 2D
-        cam_space_proj_point = (cam_space_proj_point[0], -1*cam_space_proj_point[1])     # flips image because of pygame
-        cam_space_proj_point = va(sm(7, cam_space_proj_point), sm(.5, dims))    # centers points on screen
-        return cam_space_proj_point
-
-
-def display_verts(show_global_vert=False):
-    for vertex_index in range(len(vertices)):
-        try:
-            color = vert_colors[vertex_index]
-        except KeyError:
-            color = "White"
-        if show_global_vert:
-            drawpoint(vertices[vertex_index], color)
-        drawpoint(project_to_screen(vertices[vertex_index]), color)
-
-
-def display_edges(show_global_edge=False):
-    for edge_index in range(len(edges)):
-        try:
-            color = edge_colors[edge_index]
-        except KeyError:
-            color = "White"
-        vert1, vert2 = vertices[edges[edge_index][0]], vertices[edges[edge_index][1]]
-        if show_global_edge:
-            drawline(vert1, vert2, color)
-        drawline(project_to_screen(vert1), project_to_screen(vert2), color)
 
 
 def va(vector1, vector2, vector3=(0, 0, 0), sign=1):   # vector-addition (only use sign if adding 2 vectors)
@@ -184,9 +192,15 @@ def drawpoint(position, color="White", size=5):
         pg.draw.circle(screen, color, position, size)
 
 
-center = va(window_pos, sm(0.5, dims))   # center of the screen in screen coordinates
+cam = Camera((0, 0, 100))
 
+Cube = Model([(-50, -50, 300), (50, -50, 300), (50, 50, 300), (-50, 50, 300),
+              (-50, -50, 400), (50, -50, 400), (50, 50, 400), (-50, 50, 400)],
+             [(0, 1), (1, 2), (2, 3), (3, 0), (0, 4), (1, 5), (2, 6), (3, 7), (4, 5), (5, 6), (6, 7), (7, 4)])
+
+center = va(window_pos, sm(0.5, dims))   # center of the screen in screen coordinates
 move_mouse()
+
 while True:     # main loop in which everything happens
     for event in pg.event.get():
         if event.type == pg.QUIT or pg.key.get_pressed()[pg.K_ESCAPE]:
@@ -200,10 +214,11 @@ while True:     # main loop in which everything happens
     screen.fill("Black")
 
     move_mouse()
-    move_cam()
+    cam.move_cam()
 
-    display_edges()
-    display_verts()
+    for model in models:
+        cam.display_edges(model)
+        cam.display_verts(model)
 
     # print(1/-(stime - (stime := time.time())))  # show fps
     pg.display.update()
